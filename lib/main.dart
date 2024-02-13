9c7474028c313d265fcee2fe0b25d2bc48f975e7
AldoRH <aldo.romeroh@gmail.com>
2024-02-13 10:06:41 -0600

    Update main
    Se definen las rutas de la aplicación

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:design/screens/home_screen.dart';
+import 'package:design/screens/scroll_design.dart';
 import 'package:flutter/material.dart';
 
+import 'package:design/screens/basic_design.dart';
+import 'package:flutter/services.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,17 +11,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      initialRoute: 'home_screen',
+      routes: {
+        'basic_design': (_) =>   BasicDesignScreen(),
+        'scroll_screen': (_) =>   ScrollScreen(),
+        'home_screen': (_) =>   HomeScreen(),
+      },
     );
   }
 }
